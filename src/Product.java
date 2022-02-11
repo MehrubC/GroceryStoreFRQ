adbfd8a1e7efd6bb67ba782d69123099881adafe
BT_1E10_05 <j>
2022-02-11 13:23:12 -0500

    added solution code

--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,14 @@
 public class Product
 {
     private String name;
-    private int quantity;
+    private int amount;
+    private double weight;
 
-    public Product(String name, int quantity)
+    public Product(String name, int amount, double weight)
     {
         this.name = name;
-        this.quantity = quantity;
+        this.amount = amount;
+        this.weight = weight;
     }
 
     /** Returns the name of the product */
@@ -17,8 +19,13 @@ public class Product
     /** Returns the quantity of the product that is in stock
      * Postcondition: The value returned is greater than or equal to zero.
      */
-    public int getQuantity()
+    public int getAmount()
     {
-        return quantity;
+        return amount;
+    }
+
+    public double getWeight()
+    {
+        return weight;
     }
 }
